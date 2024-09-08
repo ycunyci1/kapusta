acf233e45035a5b1f23bee64b12c7d0e1279a850
Егор <159357tdutybz99@gmail.com>
2024-09-09 02:36:29 +0300

    Доработки crud project

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Expense extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
 
+    public function imeges(): HasMany
+    {
+        return $this->hasMany(ExpenseImage::class);
+    }
 }
